c3209939248b6ec133bbd60222e7ecf4a966a28a
gustavo <gustavo.cesariosilva15@gmail.com>
2023-08-30 07:57:15 -0300

    Feat: Exercicio3 do livro Batista Moraes. Letra f. Usei operador ternario

--- BmExercicio3.java
+++ BmExercicio3.java
@@ -6,7 +6,7 @@ public class BmExercicio3 {
         double item2 = 3.50;
         double custoTotal= item1 + item2;
         double taxa = 0.825;
-
+        
         System.out.println("Item 1 custa:" + item1);
         System.out.println("Item 2 custa:" + item2);
         System.out.println("A soma dos itens é:" +custoTotal);
@@ -16,5 +16,8 @@ public class BmExercicio3 {
         System.out.println("Taxa calculada: "+ taxaCalculada);
         double novoCusto = ((item1 * taxaCalculada)+ item1)+((item2 * taxaCalculada)+item2);
         System.out.println("Novo Custo: " + novoCusto);
+        boolean muitoCaro = (novoCusto <= 10) ? false : true;
+        System.out.println(muitoCaro);
+        
     }
 }
