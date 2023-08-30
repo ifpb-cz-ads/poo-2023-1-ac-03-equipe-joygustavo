386a9a623d4881392050c791085365b46b7e852d
gustavo <gustavo.cesariosilva15@gmail.com>
2023-08-30 07:50:46 -0300

    Feat: Exercicio3 do livro Batista Moraes. Letra d) e e)

--- BmExercicio3.java
+++ BmExercicio3.java
@@ -5,9 +5,16 @@ public class BmExercicio3 {
         double item1 =  2.95;
         double item2 = 3.50;
         double custoTotal= item1 + item2;
+        double taxa = 0.825;
 
         System.out.println("Item 1 custa:" + item1);
         System.out.println("Item 2 custa:" + item2);
         System.out.println("A soma dos itens é:" +custoTotal);
+
+        double taxaCalculada = (custoTotal * taxa) + custoTotal;
+
+        System.out.println("Taxa calculada: "+ taxaCalculada);
+        double novoCusto = ((item1 * taxaCalculada)+ item1)+((item2 * taxaCalculada)+item2);
+        System.out.println("Novo Custo: " + novoCusto);
     }
 }
