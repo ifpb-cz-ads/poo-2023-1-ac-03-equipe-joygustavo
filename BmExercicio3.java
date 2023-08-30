d7ce982716a6b4a4e9ee945b2ce5de3494b0f3bb
gustavo <gustavo.cesariosilva15@gmail.com>
2023-08-30 07:43:59 -0300

    Feat: Exercicio3 do livro Batista Moraes. Letra a)

---
+++ BmExercicio3.java
@@ -0,0 +1,11 @@
+
+
+public class BmExercicio3 {
+    public static void main(String[] args) {
+        double item1 =  2.95;
+        double item2 = 3.50;
+
+        System.out.println("Item 1 custa:" + item1);
+        System.out.println("Item 2 custa:" + item2);
+    }
+}
