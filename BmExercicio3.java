28543ac021b30d04f0b49a0e783b8399ce6f9f22
gustavo <gustavo.cesariosilva15@gmail.com>
2023-08-30 07:45:20 -0300

    Feat: Exercicio3 do livro Batista Moraes. Letra b e c

--- BmExercicio3.java
+++ BmExercicio3.java
@@ -4,8 +4,10 @@ public class BmExercicio3 {
     public static void main(String[] args) {
         double item1 =  2.95;
         double item2 = 3.50;
+        double custoTotal= item1 + item2;
 
         System.out.println("Item 1 custa:" + item1);
         System.out.println("Item 2 custa:" + item2);
+        System.out.println("A soma dos itens é:" +custoTotal);
     }
 }
