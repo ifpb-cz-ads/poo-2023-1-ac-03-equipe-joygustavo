e277ba1bcb3c65de435505b3e28d7996f6671137
gustavo <gustavo.cesariosilva15@gmail.com>
2023-08-30 07:38:30 -0300

    Feat: Exercicio2 do livro Batista Moraes

---
+++ BMExercio02.java
@@ -0,0 +1,13 @@
+
+public class BMExercio02 {
+    public static void main(String[] args) { int i = 1; long j = 2L; float p = 20.0f; double q = 30.0; boolean b = true; char c = 'k';
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("p = " + p);
+        System.out.println("q = " + q);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+ 
+    }
+}
